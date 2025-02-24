124e7c0d7fb53de3c9e5cfac16dee31048776cc2
mhersent <melvin.hersent@gmail.com>
2025-02-24 15:37:16 +0100

    course update

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from stats import get_num_words
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -12,14 +20,9 @@ def main():
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"'{item['char']}: {item['num']}'")
 
     print("--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def sort_on(d):
